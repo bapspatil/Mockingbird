8dd747341fbcbf80c3bfdf0997090fa25f4ce47c
Bapusaheb Patil <bapoo1998@gmail.com>
2018-07-18 11:22:04 +0530

    Commit0

---
+++ README.md
@@ -0,0 +1,7 @@
+# <img src="./design/app_icon.png" width="3.5%">&nbsp;Mockingbird
+An Android alarm app that asks questions till you wake up.
+<br/><br/>
+**Warning**: This app might irritate a few lazy users.
+<br/><br/>
+## Screenshots
+<img src="./design/screen0.png" width="35%">
---
+++ app/src/main/java/bapspatil/mockingbird/AlarmsActivity.java
@@ -0,0 +1,13 @@
+package bapspatil.mockingbird;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarms);
+    }
+}
