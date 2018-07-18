c754dbf36baddc7f2078ab089e692d15de9d17c6
Bapusaheb Patil <bapoo1998@gmail.com>
2018-07-18 23:18:42 +0530

    Binding all views in questions screen...

--- app/src/main/java/bapspatil/mockingbird/ui/QuestionsActivity.java
+++ app/src/main/java/bapspatil/mockingbird/ui/QuestionsActivity.java
@@ -3,14 +3,29 @@ package bapspatil.mockingbird.ui;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import bapspatil.mockingbird.R;
 import bapspatil.mockingbird.model.AlarmItem;
 import bapspatil.mockingbird.util.Constants;
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.Realm;
 
 public class QuestionsActivity extends AppCompatActivity {
+    @BindView(R.id.alarmTimeTextView)
+    TextView alarmTimeTextView;
+    @BindView(R.id.questionTextView)
+    TextView questionTextView;
+    @BindView(R.id.firstAnswerTextView)
+    TextView firstAnswerTextView;
+    @BindView(R.id.secondAnswerTextView)
+    TextView secondAnswerTextView;
+    @BindView(R.id.thirdAnswerTextView)
+    TextView thirdAnswerTextView;
+    @BindView(R.id.fourthAnswerTextView)
+    TextView fourthAnswerTextView;
+
     private Realm realm;
     private AlarmItem alarmItem;
 
@@ -21,5 +36,9 @@ public class QuestionsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         realm = Realm.getDefaultInstance();
         alarmItem = getIntent().getParcelableExtra(Constants.ALARMITEM_KEY);
+
+        if(alarmItem != null) {
+            alarmTimeTextView.setText(alarmItem.getFriendlyTimeSet());
+        }
     }
 }
